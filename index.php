1204e818999881dfd90343048c9a723026096d62
Floraaurelie <108256556+Floraaurelie@users.noreply.github.com>
2022-06-29 11:37:50 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+//je veux faire un commentaire que les utilisateurs ne verrons pas mais qui me permettra de me retrouver
+
+echo "QU'EST CE QUE PHP"; "PHP a été créé en 1994 et est l'une des technologies 
+fondamentales du développement Web moderne. Avec toutes les nouvelles technologies utilisées 
+aujourd'hui, 
+
+PHP reste l'une des technologies côté serveur les plus utilisées sur Internet. 
+Il fournit le code sous-jacent pour de nombreux systèmes de gestion de contenu (CMS) populaires, 
+notamment WordPress , Drupal et Joomla . Un CMS permet aux utilisateurs de créer et de mettre à jour 
+leurs propres sites Web sans avoir à écrire eux-mêmes beaucoup de code complexe.
+
+PHP fournit également le code sous-jacent denombreux sites de commerce électronique, notamment WooCommerce et Magento . Ces plateformes de commerce électronique offrent un certain nombre d'outils pour vendre des produits en ligne. De cette façon, les entreprises peuvent se concentrer sur d'autres aspects de leur activité sans avoir à mettre en œuvre une logique de 
+programmation complexe à partir de zéro.";
+#ou je veux faire encore un commentaire que les utilisateurs ne verrons pas mais qui me permettra de me retrouver
+
+echo "LES FONCTIONNALITES DE PHP";
+
+echo"PHP contient des fonctionnalités intégrées pour interagir avec les données Web, Vanilla PHP ou PHP 
+sans aucun autre outil, peut être utilisé seul pour créer des back-ends d'applications Web. 
+Mais nous n'avons pas à réinventer la roue à chaque fois ! 
+Une fois que nous sommes à l'aise avec les bases du langage PHP, 
+nous avons notre sélection de frameworks PHP puissants parmi lesquels choisir ! 
+Ces frameworks fournissent un échafaudage et des solutions aux problèmes courants du développementWeb back-end. 
+Certains frameworks PHP populaires sont Laravel , CakePHP et 
+Symfony."; 
+/*je veux  encore faire cette fois ci plusieurs ligne de commentaire que les utilisateurs 
+ne verrons pas mais qui me permettra de me retrouver lorsque je finis je */
+
+ echo"COMMENT PHP EST-IL UTILISE EN HTML ?";
+
+
+
+
